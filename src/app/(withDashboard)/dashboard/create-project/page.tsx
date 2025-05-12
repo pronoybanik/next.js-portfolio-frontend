41a7ae4c9cafce3d2c4a49445a0ecba3b761731d
PRONOY <pronoybanik82@gmail.com>
2025-05-12 12:37:31 +0600

    solve error

--- src/app/(withDashboard)/dashboard/create-project/page.tsx
+++ src/app/(withDashboard)/dashboard/create-project/page.tsx
@@ -74,7 +74,7 @@ const CreateProject = () => {
       };
 
       const projectResponse = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}api/project`,
+        `${process.env.NEXT_PUBLIC_API_URL}/api/project`,
         {
           method: "POST",
           headers: {
